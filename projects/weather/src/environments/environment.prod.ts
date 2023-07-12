709b4686d1f9660397862b057c46bea5feb35349
Swatantra Kumar <rajswatantra9@gmail.com>
2023-07-12 16:05:42 +0530

    remove weather project form libary

--- projects/weather/src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: false,
-  baseUrl: 'https://omoknow.com/rest/',
-  serverhost : "http://localhost:8104/rest/",
-  // serverhost : 'https://sitserverpharmalinkage.pharmalinkage.com/rest/',
-  appName: "pharmalinkage",
-  appId: "ELABS",
-};
