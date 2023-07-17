7c2ba2744de30ad78659ca2bec0f2ce3833e5a06
Swatantra Kumar <rajswatantra9@gmail.com>
2023-07-17 19:16:51 +0530

    update ui core tree view interfacce

--- projects/my-lib/src/lib/services/tree-component/interface.ts
+++ projects/my-lib/src/lib/services/tree-component/interface.ts
@@ -10,16 +10,3 @@
     "_id":string;
     "action":any;
   }
-  
-  /** Flat to-do item node with expandable and level information */
-  export class TodoItemFlatNode {  
-    "item": string;
-    "level": number;
-    "expandable": boolean;
-    "reference":any;
-    "type":string;
-    "pId":string;
-    "pIndex":string;
-    "_id":string;
-    "action":any;
-  }
