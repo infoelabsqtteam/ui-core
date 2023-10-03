2ba6534fa84cee4c5f2d2c7a0da0331f3474033a
Swatantra Kumar <rajswatantra9@gmail.com>
2023-10-03 17:11:44 +0530

    add editor configuration in library

--- projects/my-lib/src/public-api.ts
+++ projects/my-lib/src/public-api.ts
@@ -35,5 +35,6 @@ export * from './lib/shared/enums/keycodes.enum';
 export * from './lib/services/data-share/auth-data-share/auth-data-share.service';
 export * from './lib/services/data-share/common-app-data-share/common-app-data-share.service';
 export * from './lib/services/fileHandler/file-handler.service';
+export * from './lib/shared/configuration/editor-config';
 
 
