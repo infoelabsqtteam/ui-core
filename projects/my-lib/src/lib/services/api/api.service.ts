fe7af11424fe630f80e88794e8bd27faa470c4b7
Swatantra Kumar <rajswatantra9@gmail.com>
2023-11-30 10:08:15 +0530

    remove un used code form api service

--- projects/my-lib/src/lib/services/api/api.service.ts
+++ projects/my-lib/src/lib/services/api/api.service.ts
@@ -213,20 +213,6 @@ GetTempData(payload:any){
     )
   }
 }
-// GetAllTemplate(payload:any){
-//   let api = this.envService.getApi('GET_CUSTOM_TEMPLATE');
-//   this.http.post(api, payload).subscribe(
-//     (respData:any) => {
-//         if(respData && respData.length > 0){
-//           let preparedTempList = this.coreFunctionService.prepareTemplate(respData);
-//           this.stroageService.storeAllTemplate(preparedTempList)
-//         }
-//       },
-//     (error) => {
-//         console.log(error);
-//       }
-//   )
-// }
 resetTempData(){
   this.dataShareService.shareTempData([])
 }
