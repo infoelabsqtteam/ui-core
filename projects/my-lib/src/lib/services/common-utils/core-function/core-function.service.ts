52dcd779d8f22b06aef2b1378ae521d9ec20fdf9
kalki39 <kalkiram39@gmail.com>
2024-02-09 17:56:12 +0530

    tab_details_added

--- projects/my-lib/src/lib/services/common-utils/core-function/core-function.service.ts
+++ projects/my-lib/src/lib/services/common-utils/core-function/core-function.service.ts
@@ -35,6 +35,9 @@ export class CoreFunctionService {
               let menu = menuMap[mekey];
               if(menu && menu.details){
                 menuobj = menu.details;
+                if(menu?.templateTabMap){
+                  menuobj={...menuobj,templateTabs:Object.keys(menu.templateTabMap)};
+                }
               }
               let submenuList:any = [];
               if(menu && menu.submenuMap){
@@ -45,6 +48,9 @@ export class CoreFunctionService {
                     let submenu = submenuMap[smkey];
                     if(submenu && submenu.details){
                       submenuObj = submenu.details;
+                      if(submenu?.templateTabMap){
+                        submenuObj={...submenuObj,templateTabs:Object.keys(submenu.templateTabMap)};
+                      }
                     }
                     this.setTabOrPermission(submenu,permissionList);
                     submenuList.push(submenuObj);
