7deebe6cd4769f1f46b1e5e9733a38bd58e8e182
Swatantra Kumar <rajswatantra9@gmail.com>
2024-04-11 18:38:50 +0530

    update operator class

---
+++ projects/my-lib/src/lib/shared/enums/operator_type.enum.ts
@@ -0,0 +1,11 @@
+export enum OperatorType {
+  STRING="string",
+  NUMBER="number",
+  DATE="date",
+  // Add more types as needed
+}
+
+export enum OperatorKey {
+  SHORTNAME = "shortName",
+  NAME = "name"
+}
