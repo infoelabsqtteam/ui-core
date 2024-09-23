2214eaf8cf03462817463719db8f462876f022e1
kwebdev <kunalwebdeveloper11@gmail.com>
2024-09-23 11:24:35 +0530

    Ressolve APP blocker issue by commenting the code of speacking voice , as it is not in use on WEB.

--- projects/my-lib/src/lib/services/notify/notification.service.ts
+++ projects/my-lib/src/lib/services/notify/notification.service.ts
@@ -8,23 +8,25 @@ export class NotificationService {
 
   horizontalPosition: MatSnackBarHorizontalPosition = 'right';
   verticalPosition: MatSnackBarVerticalPosition = 'top';
-  private synth: SpeechSynthesis;
-  private voices: SpeechSynthesisVoice[];
-  private selectedLanguage: string;
+  /* DO not just unComment below code related to speech, It will cause HUGE Impact on Mobile APP */
+  // private synth: SpeechSynthesis;
+  // private voices: SpeechSynthesisVoice[];
+  // private selectedLanguage: string;
 
   constructor(
     private _snackBar: MatSnackBar,
     private apiService: ApiService,
   ) {
-    this.synth = window.speechSynthesis;
-    this.voices = [];
-    this.selectedLanguage = 'en-US'; // Default language
+    /* DO not just unComment below code related to speech, It will cause HUGE Impact on Mobile APP */
+    // this.synth = window.speechSynthesis;
+    // this.voices = [];
+    // this.selectedLanguage = 'en-US'; // Default language
 
     // Load voices asynchronously
-    this.loadVoices();
-    if (speechSynthesis.onvoiceschanged !== undefined) {
-      speechSynthesis.onvoiceschanged = this.loadVoices.bind(this);
-    }
+    // this.loadVoices();
+    // if (speechSynthesis.onvoiceschanged !== undefined) {
+    //   speechSynthesis.onvoiceschanged = this.loadVoices.bind(this);
+    // }
   }
 
 
@@ -50,44 +52,45 @@ export class NotificationService {
         this.apiService.SaveFormData(payload);
     }
   }
-  setLanguage(language: string): void {
-    this.selectedLanguage = language;
-  }
-  private getVoiceByLanguage(language: string): SpeechSynthesisVoice | null {
-    return this.voices.find(voice => voice.lang === language) || null;
-  }
-  private loadVoices(): void {
-    this.voices = this.synth.getVoices();
-  }
-  private getFemaleVoice(): SpeechSynthesisVoice | null {
-    // You can improve this function to better identify female voices
-    return this.voices.find((voice:any) => voice.name.includes('female') || voice.name.includes('Female') || voice.gender === 'female') || null;
-  }
-  speak(message: string): void {
-    if (this.synth.speaking) {
-      console.error('speechSynthesis.speaking');
-      return;
-    }
+  /* DO not just unComment below code related to speech, It will cause HUGE Impact on Mobile APP */
+  // setLanguage(language: string): void {
+  //   this.selectedLanguage = language;
+  // }
+  // private getVoiceByLanguage(language: string): SpeechSynthesisVoice | null {
+  //   return this.voices.find(voice => voice.lang === language) || null;
+  // }
+  // private loadVoices(): void {
+  //   this.voices = this.synth.getVoices();
+  // }
+  // private getFemaleVoice(): SpeechSynthesisVoice | null {
+  //   // You can improve this function to better identify female voices
+  //   return this.voices.find((voice:any) => voice.name.includes('female') || voice.name.includes('Female') || voice.gender === 'female') || null;
+  // }
+  // speak(message: string): void {
+  //   if (this.synth.speaking) {
+  //     console.error('speechSynthesis.speaking');
+  //     return;
+  //   }
 
-    if (message !== '') {
-      const utterThis = new SpeechSynthesisUtterance(message);
-      const selectedVoice = this.getFemaleVoice();
-      // const selectedVoice = this.getVoiceByLanguage(this.selectedLanguage);
+  //   if (message !== '') {
+  //     const utterThis = new SpeechSynthesisUtterance(message);
+  //     const selectedVoice = this.getFemaleVoice();
+  //     // const selectedVoice = this.getVoiceByLanguage(this.selectedLanguage);
 
-      if (selectedVoice) {
-        utterThis.voice = selectedVoice;
-      }
+  //     if (selectedVoice) {
+  //       utterThis.voice = selectedVoice;
+  //     }
 
-      utterThis.onend = () => {
-        console.log('SpeechSynthesisUtterance.onend');
-      };
+  //     utterThis.onend = () => {
+  //       console.log('SpeechSynthesisUtterance.onend');
+  //     };
 
-      utterThis.onerror = (event) => {
-        console.error('SpeechSynthesisUtterance.onerror', event);
-      };
+  //     utterThis.onerror = (event) => {
+  //       console.error('SpeechSynthesisUtterance.onerror', event);
+  //     };
 
-      this.synth.speak(utterThis);
-    }
-  }
+  //     this.synth.speak(utterThis);
+  //   }
+  // }
 
 }
